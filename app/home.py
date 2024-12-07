ac83136d1436fada26f2dcd7fd84736b738a66cb
Bryan Galindo <galindo.bryan08@gmail.com>
2024-12-07 15:17:54 -0600

    update imports to pep8 standard

--- app/home.py
+++ app/home.py
@@ -1,12 +1,10 @@
 import logging
 import os
-import trino
-import pandas as pd
-
-import streamlit as st
-
 
 import dotenv
+import pandas as pd
+import streamlit as st
+import trino
 
 dotenv.load_dotenv("./.env")
 
@@ -59,33 +57,44 @@ def main():
 
     st.title("Resale Handbag Market Analytics")
 
-    st.markdown("""
+    st.markdown(
+        """
     ## 👋 Introduction
     My name is **Bryan Galindo**. I'm a ex-Software Engineer at Bank of America. For the capstone project, I've partnered with a small business called [Vinty](https://getvinty.com), which specializes in reselling second-hand luxury handbags. 
 
     After a couple of conversations with the business owner, here's the business problem I'm trying to solve.
-    """)
+    """
+    )
 
     # Problem Statement
     st.header("🔍 Problem Statement")
-    st.markdown("""
+    st.markdown(
+        """
     **Vinty** wants to **identify current trends in the second-hand luxury handbag resale market** to help guide their procurement department on which bags to purchase to **minimize their average product time to sell** and ultimately **increase their sales volume**. 
 
     To identify the trends, Vinty needs at least **daily snapshots** of all the handbags that are for sale on the market, along with prior sold handbag data (if available).
-    """)
+    """
+    )
 
     # Conceptual Data Model
     st.header("🧠 Conceptual Data Model")
-    st.markdown("""
+    st.markdown(
+        """
     I've created the conceptual data model below, which includes the data sources, the ingestion process + any challenges associated with them, and the KPIs we need to extract from the data models. 
 
     Due to time constraints, the initial build will only focus on two data sources: **Rebag** and **VSP**.
-    """)
-    st.image("https://i.imgur.com/PrvhRoC.png", caption="Conceptual Data Model", use_container_width=True)
+    """
+    )
+    st.image(
+        "https://i.imgur.com/PrvhRoC.png",
+        caption="Conceptual Data Model",
+        use_container_width=True,
+    )
 
     # Technologies Section
     st.header("📟 Technologies")
-    st.markdown("""
+    st.markdown(
+        """
     I will be working with the following technologies. Due to time constraints, I may have to build an initial version of the pipeline using only the core technologies below.
 
     ### Core Technologies
@@ -97,14 +106,22 @@ def main():
     - **dbt**
     - **Trino** (managed by Starburst)
     - **Tableau**
-    """)
+    """
+    )
 
     # System Design
     st.header("🖥️ System Design")
-    st.markdown("""
+    st.markdown(
+        """
     This is the ideal system design for the capstone, although I may have to further simplify this design due to time constraints. The primary goal should be to get an initial build out on time, rather than worry about using the best tech and optimizations.
-    """)
-    st.image("https://i.imgur.com/i4PU4dy.png", caption="System Design", use_container_width=True)
+    """
+    )
+    st.image(
+        "https://i.imgur.com/i4PU4dy.png",
+        caption="System Design",
+        use_container_width=True,
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
