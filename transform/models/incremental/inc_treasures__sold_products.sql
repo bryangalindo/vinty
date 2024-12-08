7d1f44230e9827a0465cab983032c9d09716d40d
Bryan Galindo <galindo.bryan08@gmail.com>
2024-12-07 18:54:43 -0600

    add treasures incremental models

---
+++ transform/models/incremental/inc_treasures__sold_products.sql
@@ -0,0 +1,32 @@
+{% set today = var("TODAY") %}
+{% set yesterday = var("YESTERDAY") %}
+
+
+WITH today AS (
+    SELECT *
+    FROM {{ ref('stg_treasures__products') }}
+    WHERE ingestion_date = '{{ today }}'
+), yesterday AS (
+    SELECT *
+    FROM {{ ref('stg_treasures__products') }}
+    WHERE ingestion_date = '{{ yesterday }}'
+)
+
+select
+    yday.id as id,
+    yday.vendor as vendor,
+    yday.title AS title,
+    yday.price AS price,
+    yday.product_type AS product_type,
+    yday.days_on_market AS days_on_market,
+    yday.store AS store,
+    '{{ today }}' AS sold_date
+from
+    yesterday yday
+left join today tday
+on yday.id = tday.id
+where tday.id is null
+
+{% if is_incremental() %}
+    and '{{ today }}' > (select COALESCE(max(sold_date), '1970-01-01') from {{ this }})
+{% endif %}
