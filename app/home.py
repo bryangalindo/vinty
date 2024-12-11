8b4f1057df77210ab48c0ffdb68352269f7870ac
Bryan Galindo <galindo.bryan08@gmail.com>
2024-12-11 15:19:33 -0600

    update home page to add embedded presentation slides

--- app/home.py
+++ app/home.py
@@ -57,71 +57,38 @@ def main():
         initial_sidebar_state="expanded",
     )
 
-    st.title("Big Bag Data: Data-Driven Decisions for Luxury Resale")
+    st.title("📈 Big Bag Data: Data-Driven Insights for Luxury Resale")
+    body_text_1 = (
+        "My name is Bryan Galindo 👋. I'm a former Software Engineer at Bank of America. "
+        "Before venturing into my next role, I took time off to join Zach Wilson's 5-week Analytics Engineering bootcamp to learn about the modern data stack. "
+    )
+    body_text_2 = (
+        "During this brief period, I had the opportunity to work with a small business called Vinty, a secondhand luxury handbag resale company, "
+        "where I built an end-to-end, fully automated analytics platform that provided insights into the products being sold in the market."
+    )
+    body_text_3 = (
+        "As a result of the analytics platform, Vinty was able to improve its decision-making in purchase strategies "
+        "+ pricing strategies, discover how its store’s performance aligns with the industry, "
+        "and establish benchmarks for its store’s performance."
+    )
+    body_text_4 = "Feel free to see the slides below for more details or explore the rest website to see the full story behind this project!"
+    st.text(body_text_1)
+    st.text(body_text_2)
+    st.text(body_text_3)
+    st.text(body_text_4)
 
     st.markdown(
         """
-    ## 👋 Introduction
-    My name is **Bryan Galindo**. I'm a ex-Software Engineer at Bank of America. For the capstone project, I've partnered with a small business called [Vinty](https://getvinty.com), which specializes in reselling second-hand luxury handbags. 
-
-    After a couple of conversations with the business owner, here's the business problem I'm trying to solve.
-    """
-    )
-
-    # Problem Statement
-    st.header("🔍 Problem Statement")
-    st.markdown(
-        """
-    **Vinty** wants to **identify current trends in the second-hand luxury handbag resale market** to help guide their procurement department on which bags to purchase to **minimize their average product time to sell** and ultimately **increase their sales volume**. 
-
-    To identify the trends, Vinty needs at least **daily snapshots** of all the handbags that are for sale on the market, along with prior sold handbag data (if available).
-    """
-    )
-
-    # Conceptual Data Model
-    st.header("🧠 Conceptual Data Model")
-    st.markdown(
-        """
-    I've created the conceptual data model below, which includes the data sources, the ingestion process + any challenges associated with them, and the KPIs we need to extract from the data models. 
-
-    Due to time constraints, the initial build will only focus on two data sources: **Rebag** and **VSP**.
-    """
-    )
-    st.image(
-        "https://i.imgur.com/PrvhRoC.png",
-        caption="Conceptual Data Model",
-        use_container_width=True,
-    )
-
-    # Technologies Section
-    st.header("📟 Technologies")
-    st.markdown(
-        """
-    I will be working with the following technologies. Due to time constraints, I may have to build an initial version of the pipeline using only the core technologies below.
-
-    ### Core Technologies
-    - **Python**
-    - **Apache Airflow** (managed by Astronomer)
-    - **Apache Iceberg**
-    - **Apache Spark**
-    - **Amazon S3**
-    - **dbt**
-    - **Trino** (managed by Starburst)
-    - **Tableau**
-    """
-    )
-
-    # System Design
-    st.header("🖥️ System Design")
-    st.markdown(
-        """
-    This is the ideal system design for the capstone, although I may have to further simplify this design due to time constraints. The primary goal should be to get an initial build out on time, rather than worry about using the best tech and optimizations.
-    """
-    )
-    st.image(
-        "https://i.imgur.com/i4PU4dy.png",
-        caption="System Design",
-        use_container_width=True,
+    <div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%;
+     padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+     border-radius: 8px; will-change: transform;">
+      <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+        src="https://www.canva.com/design/DAGY0jztmXk/xtqDdOjjCnesrV-xemr1LQ/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+      </iframe>
+    </div>
+    <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGY0jztmXk&#x2F;xtqDdOjjCnesrV-xemr1LQ&#x2F;view?utm_content=DAGY0jztmXk&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Vintage Handbag Analytics</a> by Bryan Galindo
+        """,
+        unsafe_allow_html=True,
     )
 
 
